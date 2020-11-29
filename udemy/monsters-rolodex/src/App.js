d7d3672099ebb80858df3cc3fc8ca5d8d0884f29
Jose <precado999@gmail.com>
2020-11-29 10:17:09 -0800

    progressing react course

---
+++ udemy/monsters-rolodex/src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <p>my name is jose</p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      string: "chinguen a su madre todos",
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p> */}
+          <p>{this.state.string}</p>
+          <button onClick={() => this.setState({ string: "this is react" })}>
+            click me
+          </button>
+          {/* <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
