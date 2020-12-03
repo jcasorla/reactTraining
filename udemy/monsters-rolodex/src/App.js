1f4f261c3bf2bc0045e34cd653b4917e87081bb5
Jose <precado999@gmail.com>
2020-12-03 12:49:09 -0800

    monstersv1

--- udemy/monsters-rolodex/src/App.js
+++ udemy/monsters-rolodex/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
 
 // function App() {
@@ -59,9 +60,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList name="Jose">
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
+
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
    
